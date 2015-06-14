fa9146880d65b505ce5425540d2ecd70114e5624
bote795 <nbotell2@gmail.com>
2015-06-14 01:24:13 -0400

    added See More feature
    added a See More click whenever the text is to long so that it doesnt
    clog up the list of things to do

--- js/popup.js
+++ js/popup.js
@@ -38,6 +38,8 @@ chrome.runtime.onMessage.addListener(
     }
 
 });
+//----------------UI---------------------------
+//adds the elements dynamically
 function populateTable(){
 	var Tasks=taskManager.load();
 	var $tbody= $("tbody");
@@ -47,19 +49,45 @@ function populateTable(){
 	};
 	addJquerys();
 }
+//adds data inside the popover
+function popover () {
+	var menu = "<a class='btn btn-xs btn-danger pull-right' role='button' data-toggle='popover' title='' data-placement='left'><span class='glyphicon glyphicon-option-horizontal' aria-hidden='true'></span></a>";
+	return menu;
+}
 function MenuCreation () {
 	var dataInMenu="<button type='button' href='#' class='btn btn-primary btn-xs pull-right delete'>Delete</a>";
 	return dataInMenu;
 }
+
+//creates ReadMe
+function readMe (data,id) {
+	
+   var temp =data.substring(0,40) +"<a data-toggle='collapse' data-target='#"+id+"'> See More..." + popover() +"</a>"+
+  "<div id='"+id+"' class='collapse'>"+
+    data.substring(50,data.length-1)+
+  "</div>"
+  return temp;
+}
 function createRow(data, id)
 {
-	var menu = "<a class='btn btn-xs btn-danger pull-right' role='button' data-toggle='popover' title='' data-placement='left'>Menu</a>";
-
-	return "<tr>"+
-			"<td data-value="+id+">" + data +
-				menu +
-			"</td>"+
+	
+	var row="<tr>"+
+			"<td data-value="+id+">"; 
+	 		//if more than 50 make it have a ReadMore collapse
+	 		//if doesnt fit in one line add a readmore so it dsnt cloge up UI
+			if (data.length > 50) 
+			{
+				row += readMe(data,id);
+			}
+			else
+			{	
+				row +=data; 
+				row += popover();
+			}
+			
+			row +="</td>"+
 			"</tr>";
+	return row;
 }
 //function usd to reload all functons that are used by
 //the items generated dynamically
