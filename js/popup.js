05570e20fa0586561734c734677914112bd9c6db
bote795 <nbotell2@gmail.com>
2015-08-25 10:59:23 -0500

    add doc in popup.js

--- js/popup.js
+++ js/popup.js
@@ -8,6 +8,7 @@ jQuery(function( $ ) {
 		}
 	});
 }
+//loads everything when pop up loads
 document.body.onload = function() {
 	UI.populateTable($);
 	UI.autoClickSave($);
@@ -15,13 +16,16 @@ document.body.onload = function() {
 	UI.checkEvent($);
 	UI.editButton($);
 }
+//function called when add task button is clicked
 document.getElementById("add").onclick = function() {
 	var d = taskManager.getTask();
 	taskManager.add(d);
 }
+//called when wrench is clicked
 document.getElementById("options").onclick = function() {
 	chrome.runtime.openOptionsPage();
 }
+//delete all call
 document.getElementById("deleteAll").onclick = function() {
 	taskManager.deleteAll();
 }
